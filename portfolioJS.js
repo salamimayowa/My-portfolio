717e95ebb36e21325639a4823dd1dfd5ec1c6cca
mayowa-10 <salamiabdulbasit123@gmail.com>
2024-09-28 21:38:32 +0100

    my portfolio

---
+++ portfolioJS.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for your message, Abdulbasit will get back to you soon!");
+});
